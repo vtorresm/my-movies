ba08405d192b40cb9f54df3059ed39a1deff8c20
vtorresm <vatorresm88@gmail.com>
2023-03-20 11:28:13 -0500

    Add Layout and Home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import api from './api/axiosConfig';
+import Layout from './components/Layout';
+import Home from './components/home/Home';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+
+      console.log(response.data);
+
+      setMovies(response.data);
+    } catch (error) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
